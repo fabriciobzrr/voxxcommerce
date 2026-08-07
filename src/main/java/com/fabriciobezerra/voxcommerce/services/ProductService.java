e1829822b77710fb46ecbeb9db0a407ed5648190
Fabricio Bezerra <fabriciofrontdev@gmail.com>
2026-08-07 16:03:14 -0300

    update ProductService

--- src/main/java/com/fabriciobezerra/voxcommerce/services/ProductService.java
+++ src/main/java/com/fabriciobezerra/voxcommerce/services/ProductService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 public class ProductService {
 
@@ -17,8 +15,7 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Optional<Product> result = repository.findById(id);
-        Product product = result.get();
+        Product product = repository.findById(id).get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
