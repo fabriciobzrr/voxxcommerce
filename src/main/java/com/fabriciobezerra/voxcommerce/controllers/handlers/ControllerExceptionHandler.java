1c672feb57d67a326645a6cfe70f16b1c17d9fff
Fabricio Bezerra <fabriciofrontdev@gmail.com>
2026-08-18 02:01:34 -0300

    Bean Validation

--- src/main/java/com/fabriciobezerra/voxcommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/fabriciobezerra/voxcommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.fabriciobezerra.voxcommerce.controllers.handlers;
 
 import com.fabriciobezerra.voxcommerce.dto.CustomError;
+import com.fabriciobezerra.voxcommerce.dto.ValidationError;
 import com.fabriciobezerra.voxcommerce.services.exceptions.DatabaseException;
 import com.fabriciobezerra.voxcommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +28,18 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_CONTENT;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(status).body(err);
     }
 }
